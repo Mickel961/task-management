a819fc7ac4879b0cf5bbf627cef73027e48562ce
Mickel <75254162+Mickel961@users.noreply.github.com>
2024-03-09 18:03:20 +0200

    body validation

--- controllers/taskController.py
+++ controllers/taskController.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, abort
 from models.task import Task, db
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -14,9 +14,16 @@ def get_tasks():
 @task_bp.route('/tasks/create', methods=['POST'])
 def create_task():
     data = request.json
+
+    if 'title' not in data or 'description' not in data:
+        abort(400, "Title and description are required fields.")
+
     title = data.get('title')
     description = data.get('description')
     completed = data.get('completed', False)
+
+    if not isinstance(title, str) or not isinstance(description, str):
+        abort(400, "Title and description must be strings.")
 
     try:
         new_task = Task(title=title, description=description, completed=completed)
@@ -26,6 +33,7 @@ def create_task():
     except SQLAlchemyError as e:
         db.session.rollback()
         return jsonify({'error': str(e)}), 500
+0
 
 # Get Task by id
 @task_bp.route('/tasks/<int:task_id>', methods=['GET'])
@@ -38,16 +46,24 @@ def get_task(task_id):
 def update_task(task_id):
     task = Task.query.get_or_404(task_id)
     data = request.json
-    task.title = data.get('title', task.title)
-    task.description = data.get('description', task.description)
-    task.completed = data.get('completed', task.completed)
+
+    title = data.get('title', task.title)
+    description = data.get('description', task.description)
+    completed = data.get('completed', task.completed)
+
+    if not isinstance(title, str) or not isinstance(description, str):
+        abort(400, "Title and description must be strings.")
 
     try:
+        task.title = title
+        task.description = description
+        task.completed = completed
         db.session.commit()
         return jsonify(task.to_dict())
     except SQLAlchemyError as e:
         db.session.rollback()
         return jsonify({'error': str(e)}), 500
+
 
 # Delete Task
 @task_bp.route('/tasks/delete/<int:task_id>', methods=['DELETE'])
@@ -56,7 +72,7 @@ def delete_task(task_id):
     try:
         db.session.delete(task)
         db.session.commit()
-        return jsonify({'message': 'Task Deleted'}), 204
+        return jsonify({'message': 'Task Deleted'}), 200
     except SQLAlchemyError as e:
         db.session.rollback()
         return jsonify({'error': str(e)}), 500
